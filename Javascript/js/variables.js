c0d506372b3dbd88e69f88d21f798e8b34a2f856
Poipurin <b.pinorojo@gmail.com>
2023-01-23 12:19:31 -0300

    Variables y contenedores let

---
+++ Javascript/js/variables.js
@@ -0,0 +1,15 @@
+"use strict"
+//nombre descriptivo y corto para su reutilización
+var nombre = "Bárbara";
+
+//alcance scope
+console.log(nombre)
+function saludo(){
+    let nombre = 'Muffin';
+    console.log(nombre)
+
+    let edad = 29;
+    console.log(edad)
+}
+
+saludo();
