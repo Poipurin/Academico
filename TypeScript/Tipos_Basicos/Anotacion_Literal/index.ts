31e287bacc9795b574c6fc68ede2da348b5a607a
Poipurin <b.pinorojo@gmail.com>
2023-01-24 19:20:12 -0300

    tipos básicos

---
+++ TypeScript/Tipos_Basicos/Anotacion_Literal/index.ts
@@ -0,0 +1,4 @@
+let mensaje:string;
+//mensaje = 10; debe ser string
+
+const prueba:number = 10;
---
+++ TypeScript/Tipos_Basicos/Inferencia_de_Tipos/index.ts
@@ -0,0 +1,7 @@
+let mensaje2:string = "prueba"
+
+const prueba = 100;
+
+class Persona{};
+
+let persona = new Persona();
