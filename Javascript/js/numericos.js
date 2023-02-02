d7bd9e2a5c27ce49248c23759c601ee5bcf2d817
Poipurin <b.pinorojo@gmail.com>
2023-02-02 12:56:06 -0300

    Datos numéricos

---
+++ Javascript/js/numericos.js
@@ -0,0 +1,7 @@
+"use strict"
+var edad = 35; //numérico
+var cantidad = "100";  //string
+
+var nuevaCantidad = Number(cantidad); //convertir a número
+parseInt()//Convertir a entero
+parseFloat()//Convertir a decimal
