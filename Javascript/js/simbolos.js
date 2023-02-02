3c5142863f07b82300b3ada28169af4114caaf90
Poipurin <b.pinorojo@gmail.com>
2023-02-02 13:21:05 -0300

    Símbolos

---
+++ Javascript/js/simbolos.js
@@ -0,0 +1,9 @@
+"use strict"
+//valores únicos e inmutables
+
+var simbolo1 = Symbol();
+var simbolo2 = Symbol();//valores completamente distintos
+//simbolo1 === simbolo2 ->false
+
+
+var ambiente = Symbol('dev'); //símbolo de desarrollo
