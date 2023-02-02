946a0b835fec54f5fd2f4b668a6919a0ac7d47af
Poipurin <b.pinorojo@gmail.com>
2023-02-02 12:59:52 -0300

    Cadenas de texto

---
+++ Javascript/js/CadenasDeTexto.js
@@ -0,0 +1,8 @@
+"use strict"
+var bebida = "agua";//pueden ser comillas dobles o simples
+var comida = 'ceviche';
+
+var instruccion = "El platillo se llama 'ceviche' "; //anidando comillas
+
+var edad = 34;
+var edadtxt = String(edad); //Convertir a String
