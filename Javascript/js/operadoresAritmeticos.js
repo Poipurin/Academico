f2d9565beac969b2107f49d1361c97560d2f12cb
Poipurin <b.pinorojo@gmail.com>
2023-02-02 14:10:19 -0300

    Operadores aritméticos

---
+++ Javascript/js/operadoresAritmeticos.js
@@ -0,0 +1,36 @@
+"use strict"
+
+//Operadores aritméticos
+
+var datoA = 10;
+var datoB = 20;
+
+//SUMA +
+var suma = datoA + datoB;
+console.log('la suma de '+ datoA +' + '+ datoB +' es igual a: ', suma);
+
+//RESTA -
+var resta = datoA - datoB;
+console.log('la resta de '+ datoA +' - '+ datoB +' es igual a: ', resta);
+
+//MULTIPLICACIÓN *
+var multiplicacion = datoA * datoB;
+console.log('la multiplicación de '+ datoA +' * '+ datoB +' es igual a: ', multiplicacion);
+
+//DIVISION /
+var division = datoA / datoB;
+console.log('la división de '+ datoA +' / '+ datoB +' es igual a: ', division);
+
+// MODULO O RESIDUO %
+var modulo = datoA % datoB;
+console.log('el módulo o resto de '+ datoA +' % '+ datoB +' es igual a: ', modulo);
+
+//INCREMENTO ++
+var incremento = datoA;
+incremento++;
+console.log('el incremento ++ de '+ datoA +' es igual a ', incremento);
+
+//DECREMENTO --
+var decremento = datoA;
+decremento--;
+console.log('el decremento -- de '+ datoA +' es igual a ', decremento);
