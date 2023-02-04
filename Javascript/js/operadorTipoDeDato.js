e6c4e853b8e8dbdf2e679a3caa1905ee2ddbe079
Poipurin <b.pinorojo@gmail.com>
2023-02-03 21:19:51 -0300

    Operador Typeof

---
+++ Javascript/js/operadorTipoDeDato.js
@@ -0,0 +1,21 @@
+"use strict"
+//Operador tipo de datos
+
+var datoA = 10; //Número
+
+var nombre = "Playa"; //Cadena de texto
+
+var activo = true; // Booleano
+
+var persona = {
+    edad: 34, //Número
+    deporte: 'Correr'// Cadena de texto
+} //Objeto
+
+console.log(typeof datoA);
+console.log(typeof nombre);
+console.log(typeof activo);
+console.log(typeof persona);
+//propiedades del objeto
+console.log(typeof persona.edad);
+console.log(typeof persona.deporte);
