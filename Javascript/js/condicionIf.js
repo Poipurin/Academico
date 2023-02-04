553f6e0d5a476bac8bbc9410c2f65884ac4fcd8f
Poipurin <b.pinorojo@gmail.com>
2023-02-03 21:39:12 -0300

    Condición if

---
+++ Javascript/js/condicionIf.js
@@ -0,0 +1,14 @@
+"use strict"
+//Condición if
+
+var datoA = 10;
+var datoB = 20;
+var resultado = "Sin datos";
+
+
+if(datoA > datoB){
+
+    resultado = "La condición se cumplió";
+}
+
+console.log("El resultado de la evaluación if es: ", resultado);
