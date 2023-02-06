4ed5543ebaeee47524ddcc41483ae6e2626f1f3f
Poipurin <b.pinorojo@gmail.com>
2023-02-06 12:50:44 -0300

    Ciclo DO WHILE

---
+++ Javascript/js/cicloDoWhile.js
@@ -0,0 +1,14 @@
+"use strict"
+
+//CICLO DO WHILE | CICLO INDEFINIDO
+//Iteración indeterminada o desconocida
+
+var productos = 5;
+//ejecutar la acción y después preguntar si podemos seguir ejecutando las acciones
+
+do{
+    console.log('Producto vendido');
+    productos--;
+    debugger;
+}//ejecución
+while (productos >= 1)//condición ¿Se puede seguir ejecutando el ciclo?
