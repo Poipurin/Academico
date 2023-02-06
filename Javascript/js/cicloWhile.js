f72b1a5703e1d1971c7378b9a6476f2953af3b99
Poipurin <b.pinorojo@gmail.com>
2023-02-06 12:42:55 -0300

    Ciclo WHILE

---
+++ Javascript/js/cicloWhile.js
@@ -0,0 +1,12 @@
+"use strict"
+
+//CICLO WHILE | CICLO INDEFINIDO
+//Iteración indeterminada o desconocida
+
+var productos = 5;
+
+while(productos > 0){
+    console.log('Producto vendido');
+    productos--;
+    debugger;
+}
