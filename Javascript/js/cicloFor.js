3a2a4648d6c6a209a95945fbf0c0978f6a4dcf92
Poipurin <b.pinorojo@gmail.com>
2023-02-06 12:37:01 -0300

    Ciclo FOR

---
+++ Javascript/js/cicloFor.js
@@ -0,0 +1,15 @@
+"use strict"
+
+//Ciclos
+//Definidos e indefinidos (sabemos o no cuántas veces se va a repetir)
+//Definidos: FOR
+//Indefinidos: WHILE y DO WHILE
+
+//CICLO FOR | CICLO DEFINIDO
+//Repetición = iteración
+
+var productos = 5;
+for(let contador = 0; contador < productos; contador++){//mientras el contador sea menor que el producto
+    console.log("Producto #"+ contador);
+    debugger;
+}
