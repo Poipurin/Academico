8c4a4e6ed328c1f4190e469cfaea3669c0ecd633
Poipurin <b.pinorojo@gmail.com>
2023-02-07 12:57:46 -0300

    Parámetros en Funciones

---
+++ Javascript/js/parametrosFuncion.js
@@ -0,0 +1,12 @@
+"use strict"
+// Parámetros de una función
+
+function saludar(nombre, edad){
+    console.log('Hola: ', nombre);
+    console.log('Edad: ', edad);
+    var resultado = nombre + " tiene " + edad + " años";
+    return resultado;
+}
+
+var mensaje = saludar("Bárbara", 29);
+console.log(mensaje);
